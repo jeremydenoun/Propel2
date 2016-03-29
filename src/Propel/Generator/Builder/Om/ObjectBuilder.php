6b2d171c10e7ac54316d119f16a5303f4c438756
Jeremy Denoun <jeremy.denoun@iguanesolutions.com>
2016-03-29 16:48:19 +0200

    tests suite w/fixture, fix object/query builder

--- src/Propel/Generator/Builder/Om/ObjectBuilder.php
+++ src/Propel/Generator/Builder/Om/ObjectBuilder.php
@@ -185,7 +185,7 @@ class ObjectBuilder extends AbstractObjectBuilder
             if (!in_array($val, $valueSet)) {
                 throw new EngineException(sprintf('Default Value "%s" is not among the enumerated values', $val));
             }
-            $defaultValue = array_search($val, $valueSet);
+            $defaultValue = (strtoupper($column->getDomain()->getSqlType()) == "TINYINT" ? array_search($val, $valueSet) : $val);
         } elseif ($column->isSetType()) {
             $defaultValue = SetColumnConverter::convertToInt($val, $column->getValueSet());
         } elseif ($column->isPhpPrimitiveType()) {
@@ -1912,7 +1912,7 @@ abstract class ".$this->getUnqualifiedClassName().$parentClass." implements Acti
             if (!in_array(\$v, \$valueSet)) {
                 throw new PropelException(sprintf('Value \"%s\" is not accepted in this enumerated column', \$v));
             }
-            ".( strtoupper($col->getDomain()->getSqlType()) == "TINYINT" ? '$v = array_search($v, $valueSet);' : "")."
+            ".(strtoupper($col->getDomain()->getSqlType()) == "TINYINT" ? '$v = array_search($v, $valueSet);' : "")."
         }
 
         if (\$this->$clo !== \$v) {
